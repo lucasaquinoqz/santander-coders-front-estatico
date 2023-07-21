83ab6ea6b633dcbb39364c8a1be3282090f79d52
lucasaquinoqz <lucasaquinoq@gmail.com>
2023-07-21 17:32:58 -0300

    Adicionando o exercicio da aula 04

---
+++ aula04/index.js
@@ -0,0 +1,4 @@
+document.getElementById("btnEnviar").addEventListener("click", 
+        function trocarAriaLabel(){
+            document.getElementById("btnEnviar").setAttribute("aria-label", "Botao para reenvio de formulario")
+        })
